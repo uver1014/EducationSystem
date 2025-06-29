cff6fedc6cd539282901c23ba1215fe12116edba
marino <marino.sakamoto32@gmail.com>
2025-06-30 08:14:35 +0900

    配信時間の処理をDeliveryTimeに移動

--- app/Models/Curriculum.php
+++ app/Models/Curriculum.php
@@ -42,24 +42,6 @@ class Curriculum extends Model{
         $this->save();
     }
 
-   // public function saveDeliveryTimes(array $fromDate, array $fromTime, array $toDate, array $toTime): void{
-   //     $this->deliveryTimes()->delete();
-
-   //     for ($i = 0; $i < count($fromDate); $i++) {
-   //         if (empty($fromDate[$i]) || empty($fromTime[$i]) || empty($toDate[$i]) || empty($toTime[$i])) {
-   //             continue;
-   //         }
-   //         $delivery = new DeliveryTime();
-   //         $delivery->curriculums_id = $this->id;
-   //         $delivery->delivery_from = Carbon::createFromFormat('Y-m-d H:i', $fromDate[$i] . ' ' . $fromTime[$i]);
-   //         $delivery->delivery_to   = Carbon::createFromFormat('Y-m-d H:i', $toDate[$i] . ' ' . $toTime[$i]);
-   //         $delivery->save();
-   //     }
-
-   //     $this->alway_delivery_flg = 0;
-   //     $this->save();
-   // }
-
     public function grade() {
         return $this->belongsTo(Grade::class, 'grade_id');
     }
